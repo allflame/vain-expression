1abb5aa39843b8e55945b0c4a04623a2968d8137
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-06-09 10:26:01 +0300

    Added ParserInterface

---
+++ src/Parser/ParserInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 6/9/16
+ * Time: 10:25 AM
+ */
+
+namespace Vain\Expression\Parser;
+
+use Vain\Expression\Visitor\VisitorInterface;
+
+interface ParserInterface extends VisitorInterface
+{
+
+}
