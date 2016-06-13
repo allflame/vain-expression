2c006815f4c301daaebbcdaf277cecc7b443a36d
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-06-13 12:10:16 +0300

    Switched to interpreter

--- src/Result/Factory/ExpressionResultFactoryInterface.php
+++ src/Result/Factory/ExpressionResultFactoryInterface.php
@@ -9,29 +9,70 @@
 namespace Vain\Expression\Result\Factory;
 
 use Vain\Core\Result\ResultInterface;
+use Vain\Expression\Boolean\AndX\AndExpression;
+use Vain\Expression\Boolean\False\FalseExpression;
+use Vain\Expression\Boolean\Identity\IdentityExpression;
+use Vain\Expression\Boolean\Not\NotExpression;
+use Vain\Expression\Boolean\OrX\OrExpression;
+use Vain\Expression\Boolean\True\TrueExpression;
+use Vain\Expression\ExpressionInterface;
 use Vain\Expression\Result\ExpressionResultInterface;
 
 interface ExpressionResultFactoryInterface
 {
     /**
-     * @param boolean $result
+     * @param FalseExpression $expression
      *
      * @return ExpressionResultInterface
      */
-    public function createZeroaryResult($result);
+    public function false(FalseExpression $expression);
 
     /**
+     * @param TrueExpression $expression
+     *
+     * @return ExpressionResultInterface
+     */
+    public function true(TrueExpression $expression);
+
+    /**
+     * @param IdentityExpression $expression
      * @param ResultInterface $result
      *
      * @return ExpressionResultInterface
      */
-    public function createUnaryResult(ResultInterface $result);
-    
+    public function id(IdentityExpression $expression, ResultInterface $result);
+
     /**
-     * @param ResultInterface $firstResult
-     * @param ResultInterface $secondResult
+     * @param NotExpression $expression
+     * @param ResultInterface $result
      *
-     * @return mixed
+     * @return ExpressionResultInterface
      */
-    public function createBinaryResult(ResultInterface $firstResult, ResultInterface $secondResult);
+    public function not(NotExpression $expression, ResultInterface $result);
+
+    /**
+     * @param ExpressionInterface $expression
+     * @param ResultInterface $result
+     *
+     * @return ExpressionResultInterface
+     */
+    public function comparison(ExpressionInterface $expression, ResultInterface $result);
+
+    /**
+     * @param AndExpression $binaryExpression
+     * @param ExpressionResultInterface $firstResult
+     * @param ExpressionResultInterface $secondResult
+     *
+     * @return ExpressionResultInterface
+     */
+    public function andX(AndExpression $binaryExpression, ExpressionResultInterface $firstResult, ExpressionResultInterface $secondResult);
+
+    /**
+     * @param OrExpression $binaryExpression
+     * @param ExpressionResultInterface $firstResult
+     * @param ExpressionResultInterface $secondResult
+     *
+     * @return ExpressionResultInterface
+     */
+    public function orX(OrExpression $binaryExpression, ExpressionResultInterface $firstResult, ExpressionResultInterface $secondResult);
 }
