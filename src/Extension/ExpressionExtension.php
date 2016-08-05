02052b683bbf1c0f04ec42a23b518696a51c2dca
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 12:24:38 +0300

    Fixed di.yml

---
+++ src/Extension/ExpressionExtension.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-expression
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-expression
+ */
+namespace Vain\Expression\Extension;
+
+use Vain\Core\Extension\AbstractExtension;
+
+/**
+ * Class ExpressionExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class ExpressionExtension extends AbstractExtension
+{
+}
